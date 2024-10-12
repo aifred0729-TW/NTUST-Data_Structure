d5ac73ab56bf5b85a0f9fd93a212e90a1184730b
Red Meow <44439821+aifred0729-TW@users.noreply.github.com>
2024-10-12 17:09:20 +0800

    feat : Finish push and pop function

--- Homework01_Double_Queue/main.c
+++ Homework01_Double_Queue/main.c
@@ -1,13 +1,43 @@
 #include <stdio.h>
+#include <stdbool.h>
+
+const int ARRAY_MAX = 30;
+const int DATA_MAX = 10;
+const int NODE = 10;
 
 typedef struct dq {
-	*double_queue meow;
-} double_queue;
+	int offset;
+	int base;
+	int value[30];
+} DOUBLE_QUEUE;
 
+void initializeDoubleQueue(DOUBLE_QUEUE* dq) {
+	dq->offset = 0;
+	dq->base = 0;
+	for (size_t i = 0; i < 20; i++) dq->value[i] = NULL;
+	return;
+}
+
+bool pushFront(DOUBLE_QUEUE* dq, int value) {
+	if (dq->offset - dq->base + 1 < DATA_MAX) return false;
+	if (dq->offset + 1 == ARRAY_MAX) return false;
+	dq->value[dq->base + dq->offset + 1] = value;
+	return true;
+}
+
+int popBack(DOUBLE_QUEUE* dq) {
+	dq->offset--;
+	return dq->value[dq->base + dq->offset+1];
+}
+
+int popFront(DOUBLE_QUEUE* dq) {
+	dq->base++;
+	return dq->value[dq->base - 1];
+}
 
 int main() {
-
-
+	DOUBLE_QUEUE dq;
+	initializeDoubleQueue(&dq);
 
 
 
